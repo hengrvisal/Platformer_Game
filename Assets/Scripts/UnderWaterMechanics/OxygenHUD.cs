24922b59909cfd9c4fd78ae7ef3044d3b35fe915
visal <heng.rvisal2@gmail.com>
2025-09-28 18:35:05 +1000

    holding breath feature working

--- Assets/Scripts/UnderWaterMechanics/OxygenHUD.cs
+++ Assets/Scripts/UnderWaterMechanics/OxygenHUD.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.Rendering.Universal.Internal;
 using UnityEngine.UI;
 
 public class OxygenHUD_Events : MonoBehaviour
@@ -7,16 +6,18 @@ public class OxygenHUD_Events : MonoBehaviour
     [SerializeField] OxygenSystem oxygen;
     [SerializeField] Slider slider;
 
-    void Awake() { if (!slider) slider = GetComponent<Slider>(); if (slider) { slider.minValue = 0; slider.maxValue = 1; } }
-    void OnEnable()
+    void Awake()
     {
+        if (!slider) slider = GetComponent<Slider>();
         if (!oxygen) oxygen = FindAnyObjectByType<OxygenSystem>();
-        if (oxygen)
+        if (slider)
         {
-            oxygen.OnOxygenChanged.AddListener(UpdateBar);
-            UpdateBar(oxygen.currentOxygen, oxygen.MaxOxygen); // initial
+            slider.minValue = 0; slider.maxValue = 1; slider.wholeNumbers = false;
         }
     }
-    void OnDisable() { if (oxygen) oxygen.OnOxygenChanged.RemoveListener(UpdateBar); }
-    void UpdateBar(float cur, float max) { slider.value = max > 0 ? cur / max : 0; }
+
+    void Update()
+    {
+        if (oxygen && slider) slider.value = oxygen.Normalized;
+    }
 }
--- Assets/Scripts/UnderWaterMechanics/OxygenSystem.cs
+++ Assets/Scripts/UnderWaterMechanics/OxygenSystem.cs
@@ -1,53 +1,37 @@
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.Rendering;
 
 public class OxygenSystem : MonoBehaviour
 {
     [SerializeField] float maxOxygen = 10f;
     [SerializeField] float drainPerSec = 1f;
     [SerializeField] float refillPerSec = 3f;
-
-    [SerializeField] SwimController swimmer;
-    public UnityEvent<float, float> OnOxygenChanged; // (current, max)
-    public UnityEvent OnDrowned;
+    [SerializeField] float surfaceY = 0f; // sea-level
 
     float oxy;
+    bool dead;
+    public UnityEvent OnDrowned = new();
 
-    public float currentOxygen => oxy;
+    public float CurrentOxygen => oxy;
     public float MaxOxygen => maxOxygen;
+    public float Normalized => maxOxygen > 0 ? oxy / maxOxygen : 0f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
-        if (!swimmer)
-        {
-            swimmer = GetComponent<SwimController>();
-        }
-
         oxy = maxOxygen;
-        OnOxygenChanged?.Invoke(oxy, maxOxygen);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        bool atSurface = swimmer && swimmer.IsAtSurface();
-        float delta = (atSurface ? +refillPerSec : -drainPerSec) * Time.deltaTime;
-        oxy = Mathf.Clamp(oxy + delta, 0f, maxOxygen);
-        OnOxygenChanged?.Invoke(oxy, maxOxygen);
+        bool atSurface = transform.position.y >= surfaceY - 0.05f;
+        float rate = atSurface ? +refillPerSec : -drainPerSec;
+        oxy = Mathf.Clamp(oxy + rate * Time.deltaTime, 0f, maxOxygen);
 
-        if (Time.frameCount % 30 == 0)
+        if (!dead && oxy <= 0f)
         {
-            Debug.Log($"O2={oxy:F2}, atSurface={atSurface}, y={transform.position.y:F2}, surfY={swimmer?.SurfaceY}");
+            dead = true;
+            OnDrowned.Invoke();
         }
-
-        if (oxy <= 0f) OnDrowned?.Invoke();
-    }
-
-    public void RefillBurst(float amount)
-    {
-        oxy = Mathf.Clamp(oxy + amount, 0f, maxOxygen);
-        OnOxygenChanged?.Invoke(oxy, maxOxygen);
     }
 }
